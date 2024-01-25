54ed4ea362dfc73ad821b347033255f000f5b722
miloskaland <miloskaland@gmail.com>
2024-01-26 01:44:07 +0900

    fix: update buying followers dialog, error handling

--- src/features/profile/api/buy-follower.ts
+++ src/features/profile/api/buy-follower.ts
@@ -48,24 +48,6 @@ export const buyfollower = async (
   }
 };
 
-export const buyfollowers = async (
-  session_owner_id: string,
-  packages: number,
-  amount: number,
-) => {
-  try {
-    const { data } = await axios.put("/api/users/buyFollowers", {
-      session_owner_id,
-      packages,
-      amount,
-    });
-
-    return data;
-  } catch (error: any) {
-    return error.message;
-  }
-};
-
 export const getPackages = async (user_id: string) => {
   try {
     const params = {
